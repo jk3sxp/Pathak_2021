5d45bee68fa7db5efb0b3fd74ee8cfaa39179e3d
jk3sxp <jk3sxp@virginia.edu>
2022-05-18 16:11:14 -0400

    may 2022

--- simulation_data/__init__.py
+++ simulation_data/__init__.py
@@ -5,7 +5,7 @@ API = "552df1d694cafaab593b6db147645204"
 #defined get()
 def get(path, params=None):
     '''
-    The oroginal version of this function can be found at
+    The original version of this function can be found at
     https://www.tng-project.org/data/docs/api/
     '''
     # make HTTP GET request to path
@@ -14,9 +14,12 @@ def get(path, params=None):
 
     # raise exception if response code is not HTTP SUCCESS (200)
     r.raise_for_status()
+    
+#     print(r.headers)
 
-    if r.headers['content-type'] == 'application/json':
-        return r.json() # parse json responses automatically
+    if 'content-type' in r.headers:
+        if r.headers['Content-Type'] == 'application/json':
+            return r.json() # parse json responses automatically
 
     if 'content-disposition' in r.headers:
         filename = r.headers['content-disposition'].split("filename=")[1]
